9688b8f16eedb20a13dcd64e71498286c83f128e
neutrixs <neutrixs@users.noreply.github.com>
2020-12-16 20:35:50 +0700

    image resize on hover

---
+++ script/imageresize.js
@@ -0,0 +1,8 @@
+function big(docid){
+    width = document.getElementById(docid).style.width;
+    document.getElementById(docid).style.width = '100%';
+}
+function small(docid){
+    width = document.getElementById(docid).style.width;
+    document.getElementById(docid).style.width = '75%';
+}
