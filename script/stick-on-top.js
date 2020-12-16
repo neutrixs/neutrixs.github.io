75a9bac82ea7097bbef61cbdd91132af22a54840
neutrixs <neutrixs@users.noreply.github.com>
2020-12-16 17:29:29 +0700

    Use template
    using template to add menu bar instead of copying it to each file(because that's no good)

---
+++ script/stick-on-top.js
@@ -0,0 +1,18 @@
+//i know this is insane but whatever
+
+document.getElementById('sticky').innerHTML = 
+`<div id="link1" onmouseout="changecolor('link1','transparent');shadow('link1','no')" onmouseover="changecolor('link1','#00cc6d');shadow('link1','yes')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+    <p><a href="../" class="white">Home</a></p>
+</div>
+<div id="link2" onmouseout="changecolor('link2','transparent');shadow('link2','no')" onmouseover="changecolor('link2','#00cc6d');shadow('link2','yes')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+    <p><a href="https://github.com/neutrixs/neutrixs.github.io" class="white" target="_blank">Source code</a></p>
+</div>
+<div id="link3" onmouseout="changecolor('link3','transparent');shadow('link3','no')" onmouseover="changecolor('link3','#00cc6d');shadow('link3','yes')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+    <p><a href="" class="white">Twitter</a></p>
+</div>
+<div id="link4" onmouseout="changecolor('link4','transparent');shadow('link4','no')" onmouseover="changecolor('link4','#00cc6d');shadow('link4','yes')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+    <p><a href="./cat_pictures.html" class="white">Cat pictures</a></p>
+</div>
+<div id="link5" onmouseout="changecolor('link5','transparent');shadow('link5','no')" onmouseover="changecolor('link5','#00cc6d');shadow('link5','yes')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+    <p><a href="https://forum.ftrix-web.xyz" class="white">Forum</a></p>
+</div>`;
