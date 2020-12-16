6b57d18b0427f5e5e48f6df72158cd66e634b4f3
neutrixs <neutrixs@users.noreply.github.com>
2020-12-16 21:04:11 +0700

    read desc
    simpler code. animations on hover and on press now uses CSS(cause i didn't know it existed)

--- script/imageresize.js
+++
@@ -1,8 +0,0 @@
-function big(docid){
-    width = document.getElementById(docid).style.width;
-    document.getElementById(docid).style.width = '100%';
-}
-function small(docid){
-    width = document.getElementById(docid).style.width;
-    document.getElementById(docid).style.width = '75%';
-}
--- script/stick-on-top.js
+++ script/stick-on-top.js
@@ -1,18 +1,18 @@
 //i know this is insane but whatever
 
 document.getElementById('sticky').innerHTML = 
-`<div id="link1" onmouseout="changecolor('link1','transparent');shadow('link1','no');inshadow('link1','no')" onmouseover="changecolor('link1','#00cc6d');shadow('link1','yes')" onmousedown="inshadow('link1','yes')" onmouseup="inshadow('link1','no')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+`<div class="divstyle noshadow fitleft marginr">
     <p><a href="../" class="white">Home</a></p>
 </div>
-<div id="link2" onmouseout="changecolor('link2','transparent');shadow('link2','no');inshadow('link2','no')" onmouseover="changecolor('link2','#00cc6d');shadow('link2','yes')" onmousedown="inshadow('link2','yes')" onmouseup="inshadow('link2','no')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+<div class="divstyle noshadow fitleft marginr">
     <p><a href="https://github.com/neutrixs/neutrixs.github.io" class="white" target="_blank">Source code</a></p>
 </div>
-<div id="link3" onmouseout="changecolor('link3','transparent');shadow('link3','no');inshadow('link3','no')" onmouseover="changecolor('link3','#00cc6d');shadow('link3','yes')" onmousedown="inshadow('link3','yes')" onmouseup="inshadow('link3','no')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+<div class="divstyle noshadow fitleft marginr">
     <p><a href="" class="white">Twitter</a></p>
 </div>
-<div id="link4" onmouseout="changecolor('link4','transparent');shadow('link4','no');inshadow('link4','no')" onmouseover="changecolor('link4','#00cc6d');shadow('link4','yes')" onmousedown="inshadow('link4','yes')" onmouseup="inshadow('link4','no')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+<div class="divstyle noshadow fitleft marginr">
     <p><a href="./cat_pictures.html" class="white">Cat pictures</a></p>
 </div>
-<div id="link5" onmouseout="changecolor('link5','transparent');shadow('link5','no');inshadow('link5','no')" onmouseover="changecolor('link5','#00cc6d');shadow('link5','yes')" onmousedown="inshadow('link5','yes')" onmouseup="inshadow('link5','no')" class="divstyle noshadow fitleft marginr" style="background-color: transparent;">
+<div class="divstyle noshadow fitleft marginr">
     <p><a href="https://forum.ftrix-web.xyz" class="white">Forum</a></p>
 </div>`;
