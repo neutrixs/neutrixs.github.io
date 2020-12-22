74db083dddcd8ee6b2384757ac1791dfaf43996b
Ihsan Rakhman <neutrixs@users.noreply.github.com>
2020-12-22 08:32:27 +0700

    k

--- script/mobile.js
+++ script/mobile.js
@@ -18,7 +18,7 @@ function smallwidth(){
     }
     
     padding0 = document.getElementsByClassName('divstyle');
-    //change i to amount of menu bar option
+    //change i to amount of menu bar option + 1
     for(var i = 6;i<padding0.length;i++){
         padding0[i].style.boxShadow = 'none';
         padding0[i].style.borderRadius = '0em';
@@ -43,7 +43,7 @@ function bigwidth(){
     }
 
     padding0 = document.getElementsByClassName('divstyle');
-    //change i to the amount of menu bar option
+    //change i to the amount of menu bar option + 1
     for(var i = 6;i<padding0.length;i++){
         padding0[i].style.boxShadow = '0.5em 0.5em 1.5em #000000, -0.5em -0.5em 1.5em #323232';
         padding0[i].style.borderRadius = '1em';
