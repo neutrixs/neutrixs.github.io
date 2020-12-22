85856fed8cb59f76da435811bc98d4081faa8a4a
Ihsan Rakhman <neutrixs@users.noreply.github.com>
2020-12-22 08:39:52 +0700

    Update stick-on-top.js
    mistake

--- script/stick-on-top.js
+++ script/stick-on-top.js
@@ -1,18 +1,18 @@
 //i know this is insane but whatever
 
 document.getElementById('sticky').innerHTML = 
-`<div id="menu" class="divstyle noshadow fitleft marginr">
+`<div name="menu" class="divstyle noshadow fitleft marginr">
     <p><a href="../" class="white">Home</a></p>
 </div>
-<div id="menu" class="divstyle noshadow fitleft marginr">
+<div name="menu" class="divstyle noshadow fitleft marginr">
     <p><a href="https://github.com/neutrixs/neutrixs.github.io" class="white" target="_blank">Source code</a></p>
 </div>
-<div id="menu" class="divstyle noshadow fitleft marginr">
+<div name="menu" class="divstyle noshadow fitleft marginr">
     <p><a href="./cat_pictures.html" class="white">Cat pictures</a></p>
 </div>
-<div id="menu" class="divstyle noshadow fitleft marginr">
+<div name="menu" class="divstyle noshadow fitleft marginr">
     <p><a href="https://forum.ftrix-web.xyz" class="white">Forum</a></p>
 </div>
-<div id="menu" class="divstyle noshadow fitleft marginr">
+<div name="menu" class="divstyle noshadow fitleft marginr">
     <p><a href="./sosmed.html" class="white">Social Media</a></p>
 </div>`;
